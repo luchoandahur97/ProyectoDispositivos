983ac17725a3e6209fe85004716dda4c256c03f4
fabian-cardenas <45020658+fabian-cardenas@users.noreply.github.com>
2019-05-29 16:49:10 -0400

    perfil.css perfil.js perfil.html

--- alertcity/www/js/my-app.js
+++ alertcity/www/js/my-app.js
@@ -1,6 +1,6 @@
 // Initialize app
 var myApp = new Framework7();
-  
+
 // If we need to use custom DOM library, let's save it to $$ variable:
 var $$ = Dom7;
 
@@ -13,17 +13,23 @@ var app = new Framework7({
     id: 'com.myapp.test',
     // Enable swipe panel
     panel: {
-      swipe: 'left',
+        swipe: 'left',
     },
     // Add default routes
-    routes: [
-      {
-        path: '/register/',
-        url: 'register.html',
-      },
-    ]
-    // ... other parameters
-  });
+    routes: [{
+                path: '/register/',
+                url: 'register.html',
+
+
+            },
+            {
+                path: '/perfil/',
+                url: 'perfil.html',
+
+            },
+        ]
+        // ... other parameters
+});
 
 var mainView = app.views.create('.view-main');
 
@@ -33,13 +39,13 @@ $$(document).on('deviceready', function() {
 });
 
 // Option 1. Using one 'page:init' handler for all pages
-$$(document).on('page:init', function (e) {
+$$(document).on('page:init', function(e) {
     // Do something here when page loaded and initialized
     console.log(e);
-})
+});
 
 // Option 2. Using live 'page:init' event handlers for each page
-$$(document).on('page:init', '.page[data-name="about"]', function (e) {
+$$(document).on('page:init', '.page[data-name="about"]', function(e) {
     // Do something here when page with data-name="about" attribute loaded and initialized
     console.log(e);
-})
+});
